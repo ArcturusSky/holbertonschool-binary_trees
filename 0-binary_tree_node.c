308253929bdc31e73ba972df27cc6dcf7d951d0e
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-06 16:34:37 +0200

    FIX : fixed my tomfoolery, kinda

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -40,20 +40,3 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	return (New_Node);
 
 }
-
-/**
- * binary_tree_is_leaf -  function that checks if a node is a leaf
- * @node: pointer to the node to test
- * Return: 1 if node is leaf, 0 otherwise
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node != NULL)
-	{
-		if (node->left == NULL && node->right == NULL)
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf -  function that checks if a node is a leaf
+ * @node: pointer to the node to test
+ * Return: 1 if node is leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL)
+	{
+		if (node->left == NULL && node->right == NULL)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
 
 /**
  * binary_tree_size - Function that count the size of a binary tree
