34dbcbbc7df775dae9cb1c810ee250d4cea75f24
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-06 16:06:28 +0200

    FIX : 13-binary_tree_nodes.c, betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,6 +4,7 @@
  * binary_tree_nodes - function that counts the nodes with
  * at least 1 child in a binary tree
  * @tree: pointer to tree root
+ * Return: N° of nodes
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -25,5 +26,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		}
 		return (node_count);
 	}
-    return (0);
+	return (0);
 }
