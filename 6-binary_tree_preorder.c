5e48446ecc5c5659703fbede837258a7ff15550a
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-06 13:26:00 +0200

    Satisfying betty

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - function that goes through a binary
+ * tree using pre-order traversal
  * @tree: pointer to tree root
  * @func: pointer to function to execute with node value
  * Return: 1 if node is root, 0 otherwise
