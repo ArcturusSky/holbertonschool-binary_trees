9df1b9b148428560353aafa13d392a4b9de8b9fc
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-07 10:15:13 +0200

    FIX : 17-binary_tree_sibling.c, fixed code to better account for NULL sibling FIX : 18-binary_tree_uncle.c, fixed code to better account for NULL uncle

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,13 +12,15 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node->parent != NULL && node != NULL)
 	{
-		if (node->parent->left == node
-		&& node->parent->right != NULL)
+		if (node->parent->left == NULL || node->parent->right == NULL)
+		{
+			return (NULL);
+		}
+		if (node->parent->left == node)
 		{
 			return (node->parent->right);
 		}
-		else if (node->parent->right == node
-		&& node->parent->left != NULL)
+		else if (node->parent->right == node)
 		{
 			return (node->parent->left);
 		}
