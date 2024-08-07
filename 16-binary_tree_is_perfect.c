ff77fadc9538eedc3b2625fe8674fb41b777fe63
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-07 12:15:18 +0200

    FIX : 16-binary_tree_is_perfect.c, ISO 90 fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -52,10 +52,10 @@ int is_perfect(const binary_tree_t *node, int depth, int level)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int depth = calculate_depth(tree);
+	
 	if (tree == NULL)
 		return (0);
-
-	int depth = calculate_depth(tree);
 
 	return (is_perfect(tree, depth, 0));
 }
